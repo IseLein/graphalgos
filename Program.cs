bb6ae650362bf9951ed1f6d5f62677f34743c31d
IseLein <iseoluwagbotemidaniel@gmail.com>
2024-01-14 03:54:54 -0500

    added site files

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+IResult getFileContent(string path, string type) {
+    string fileContent = System.IO.File.ReadAllText(path);
+    return Results.Content(fileContent, type);
+}
 
+app.MapGet("/", () => getFileContent("./site/index.html", "text/html"));
 app.Run();
